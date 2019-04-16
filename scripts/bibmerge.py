eec045b10c4daa0059cb0ca512352b20cc0583b3
Samarth <samarth.khandelwal39@gmail.com>
2019-04-16 19:50:30 +0530

    changed for scripts folder

--- scripts/bibmerge.py
+++ scripts/bibmerge.py
@@ -1,5 +1,8 @@
 import os
-all_files = os.listdir('/data/Study/Major_Project/Bibliography_Tool/uploads')
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+all_files = os.listdir(dir_path + '/..' + '/uploads')
+
 for i in all_files:
     file = open("../uploads/" + i, "r+")
     bib = file.read()
